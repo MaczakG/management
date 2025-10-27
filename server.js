b115f1003cc067f328d0146f98d8c494874eec85
MaczakG <maczakgergo@bographic.hu>
2025-10-27 21:51:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Statikus fájlok
-app.use(express.static(path.join(__dirname, "frontend")));
+// Statikus fájlok a server.js mappájából
+app.use(express.static(__dirname));
 
 // MongoDB
 const uri = "mongodb+srv://CMS_BOGRAPHIC:Kiralyok007@mgf.ym6ix.mongodb.net/?retryWrites=true&w=majority&appName=MGF";
 const client = new MongoClient(uri);
 
 async function connectDB() {
-    try {
-        await client.connect();
-        console.log("MongoDB connected!");
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await client.connect();
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.error(err);
+  }
 }
 connectDB();
 
@@ -29,19 +29,20 @@ const collection = database.collection("Partner_datas");
 
 // API endpoint
 app.get("/partners", async (req, res) => {
-    try {
-        const { name, completion } = req.query;
-        const filters = {};
-        if (name) filters.Name = { $regex: name, $options: "i" };
-        if (completion) filters.Completion_type = { $regex: completion, $options: "i" };
+  try {
+    const { name, completion } = req.query;
+    const filters = {};
+    if (name) filters.Name = { $regex: name, $options: "i" };
+    if (completion) filters.Completion_type = { $regex: completion, $options: "i" };
 
-        const documents = await collection.find(filters).toArray();
-        res.json(documents);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Hiba történt a lekérés során" });
-    }
+    const documents = await collection.find(filters).toArray();
+    res.json(documents);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Hiba történt a lekérés során" });
+  }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
