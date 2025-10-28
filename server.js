6849a47c3ac5cc06958456b221352e46b49ee324
MaczakG <maczakgergo@bographic.hu>
2025-10-28 07:25:22 +0100

    Change server port from 3000 to 80

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get("/partners", async (req, res) => {
   }
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(80, "0.0.0.0", () => {
+  console.log("Server running on port 80");
 });
+
